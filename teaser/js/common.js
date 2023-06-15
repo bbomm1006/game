439f4833960e33337acb387e167b3e5ce63b0fa6
SeoWan Kim <kimj1118@naver.com>
2023-06-15 13:48:20 +0900

    티저1차버전_수정

--- teaser/js/common.js
+++ teaser/js/common.js
@@ -19,30 +19,8 @@ const _page = {
             return false;
         });
 
-        //영상 팝업
-        $(".content_top .btn_video").on("click", function() {
-            $("#popup_video").fadeIn(200);
-            return false;
-        });
-        $("#popup_video .btn_close").on("click", function() {
-            $("#popup_video").fadeOut(200, function() {
-                $("#popup_video .video_box").html('<iframe src="https://www.youtube.com/embed/KhEeWiN__kM" frameborder="0" allowfullscreen="" class="video"></iframe>');
-            });
-            return false;
-        });
     },
     animate: function() {
         $(".animate").addClass("on");
-    }
-}
-
-//전역 명령어
-function doTimeout(name, func, timeout) {
-    var getDoName = "do_" + name;
-    if(typeof window[getDoName] !== "undefined") {
-        clearTimeout(window[getDoName]);
-    }
-    if(typeof func === "function") {
-        window[getDoName] = setTimeout(func,timeout);
     }
 }
