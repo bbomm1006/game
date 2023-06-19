44883ae718b393393daa04252df87c157d4b4ddf
bbomm1006 <bbomm1006@gmail.com>
2023-06-19 17:37:10 +0900

    티저_내부확인용_수정(공유하기)

--- teaser/js/common.js
+++ teaser/js/common.js
@@ -31,3 +31,35 @@ const _page = {
         $(".animate").addClass("on");
     }
 }
+
+//sns
+var SNS = {
+    facebook: function () {
+        var currentURl = $(location).attr('href');
+        var link = encodeURIComponent(currentURl);
+        var url = 'http://www.facebook.com/share.php?u=' + link;
+        window.open(url, '_blank', 'width=800,height=450,location=no,menubar=no,resizable=no,scrollbars=yes,status=no,toolbar=no');
+    },
+    twitter: function () {
+        var currentURl = $(location).attr('href');
+        var link = encodeURIComponent(currentURl);
+        var url = 'https://twitter.com/intent/tweet?url=' + link;
+        window.open(url, '_blank', 'width=800,height=450,location=no,menubar=no,resizable=no,scrollbars=yes,status=no,toolbar=no');
+    }
+};
+
+//주소 복사
+function urlCopy() {
+    var url = document.getElementById('text');
+    var success = true;
+    url.style.display='block';    // 숨겨둔 input 태그 block처리
+    url.select();    // 복사할 text 블럭
+    document.execCommand('copy');    // 드레그된 text 클립보드에 복사
+    url.style.display='none';    // 다시 숨기기
+    if (success) {
+        alert('주소가 복사되었습니다.\n원하는 곳에 붙여넣기(Ctrl+V) 해주세요.');
+    }
+    else {
+        alert('사용중인 브라우저가 클립 보드에 액세스를 허용하지 않습니다.\n드래그하여 복사해 주세요.');
+    }
+}
